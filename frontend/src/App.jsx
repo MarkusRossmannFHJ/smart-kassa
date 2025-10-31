f572165303fabb0e67a246034d98128bb6a54ad4
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-10-31 09:08:53 +0100

    Added some hello world functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import Confetti from 'react-confetti'
+
 export default function App() {
+
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count => count + 1);
+  }
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-16 bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-500">
       
+      <div className="flex flex-col gap-2 items-center">
+         <p>Counter: <span>{count}</span></p>
+      <button className="border p-4 rounded-lg border-orange
+      font-bold text-white hover:scale-110 transform duration-300 ease"
+       onClick={() => handleClick()}>Click me!</button>
+      </div>
+     
+
       {/* Hero Section */}
-      <section className="text-center text-white p-8 rounded-3xl shadow-2xl backdrop-blur-md bg-white/10">
+      <section className="text-center text-white p-8 
+      rounded-3xl shadow-2xl backdrop-blur-md bg-white/10
+      hover:scale-110 transform duration-300 ease">
         <h1 className="text-5xl font-extrabold mb-3 drop-shadow-lg">
           Beginning of something <span className="text-yellow-300">great...</span>
         </h1>
@@ -11,7 +31,9 @@ export default function App() {
       </section>
 
       {/* Team Section */}
-      <section className="text-center text-white p-6 rounded-2xl bg-white/10 backdrop-blur-md shadow-lg w-80">
+      <section className="text-center text-white p-6
+       rounded-2xl bg-white/10 backdrop-blur-md shadow-lg w-80
+       hover:scale-110 transform duration-300 ease">
         <h2 className="text-2xl font-semibold mb-4 drop-shadow-md">Our Team 💫</h2>
         <ul className="space-y-2 text-lg">
           {["Casper", "Markus", "Mario", "Umejr"].map((name) => (
@@ -24,6 +46,14 @@ export default function App() {
           ))}
         </ul>
       </section>
+
+        {(count === 5) && (
+             <Confetti
+      width={1000}
+      height={1000}
+    />
+        )}
+      
 
     </div>
   );
