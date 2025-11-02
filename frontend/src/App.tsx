0553da9f138580c3a95d0146be1c5c41523dd762
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2025-11-02 21:49:02 +0100

    feat: added ThemeToggle Ability under ModdleToggle.tsx Button, Protected Route to redirect unloged Users to register Page and installed Tailwind,Shadcn/ui and React-Router-Dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
+/**
+ * The Routes are all declared here
+ * @returns Router Component for Routing in the Application without reloading whole page
+ */
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,11 @@
+/**
+ * The Sign Up page, where users Sign Up
+ * @returns Register Page where Users can Sign Up
+ */
+function Register() {
+  return (
+    <main>Register</main>
+  )
+}
+
+export default Register
