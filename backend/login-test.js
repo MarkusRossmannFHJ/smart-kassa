91db38e7a751e8287862f96fb0d9ddfc23d2c35a
Markus Rossmann <markus.rossmann@edu.fh-joanneum.at>
2025-11-13 14:02:37 +0100

    chore: add script to check database output

---
+++ backend/login-test.js
@@ -0,0 +1,23 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_PUBLIC_URL,
+    ssl: { rejectUnauthorized: false },
+});
+
+async function testDB() {
+    try {
+        const result = await pool.query("SELECT * FROM account LIMIT 3");
+        console.log("Connection successful");
+        console.table(result.rows);
+    } catch (err) {
+        console.error("Error while connecting to database:", err);
+    } finally {
+        await pool.end();
+    }
+}
+
+testDB();
